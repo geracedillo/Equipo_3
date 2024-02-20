79524892fbcf5c85447bd9d64e646485d227dcc2
Cecilia Rivas <cecyrivas3@hotmail.com>
2024-02-20 17:53:46 -0600

    Cecilia Rivas

--- Equipo_3.R
+++ Equipo_3.R
@@ -1,23 +1,24 @@
 #Equipo 3
-#Cecilia Rivas González A01284874
+#Cecilia Rivas Gonz?lez A01284874
 #Gerardo Cedillo Corona A01704232
-#José Arturo Silva Flores A01198049 
-#Luis David Sánchez Castillo A01275655
-#Nayeli Peña Martínez A01368516
+#Jos? Arturo Silva Flores A01198049 
+#Luis David S?nchez Castillo A01275655
+#Nayeli Pe?a Mart?nez A01368516
 
 # Suma 
 
 
 
 #Resta 
+resta <- x -y
+resta
+
+
+#Multiplicaci?n 
 
 
 
-#Multiplicación 
-
-
-
-#División
+#Divisi?n
 
 
 
