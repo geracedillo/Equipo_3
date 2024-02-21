9d536e38b74585cf7b34b2890ef91a30124321b1
Nayeli <nayelipm100@gmail.com>
2024-02-20 18:15:42 -0600

    division

--- Equipo_3.R
+++ Equipo_3.R
@@ -1,9 +1,9 @@
 #Equipo 3
-#Cecilia Rivas González A01284874
+#Cecilia Rivas Gonz?lez A01284874
 #Gerardo Cedillo Corona A01704232
-#José Arturo Silva Flores A01198049 
-#Luis David Sánchez Castillo A01275655
-#Nayeli Peña Martínez A01368516
+#Jos? Arturo Silva Flores A01198049 
+#Luis David S?nchez Castillo A01275655
+#Nayeli Pe?a Mart?nez A01368516
 
 # Suma 
 
@@ -13,12 +13,12 @@
 
 
 
-#Multiplicación 
+#Multiplicaci?n 
 
 
 
-#División
-
-
+#Divisi?n
+division = function(){x/y}  
+division
 
 #Potencia
